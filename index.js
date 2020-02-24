b0bf9ecdf7ec1b95eb00523e66811109a93b0ba9
samuelkyprianou <samuelkyprianou@gmail.com>
2020-02-24 15:20:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions here
+const map = (array, func) => {
+  return array.map(element => func(element));
+};
+
+const reduce = (arr, func, start) => {
+  if (start) {
+    return arr.reduce(func, start);
+  } else {
+    return arr.reduce(func);
+  }
+};
